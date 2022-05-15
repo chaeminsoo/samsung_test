a08c836087547b85c866b2125003bf55c5014672
chaeminsoo <chaems97@gmail.com>
2022-05-16 00:13:47 +0900

    Greedy

--- kayak_and_wind.py
+++ kayak_and_wind.py
@@ -1,29 +1,23 @@
 # 2891
 n,s,r = map(int,input().split())
-kayaks = [1]*n
+kayaks = [1]*(n+2)
 broken_kayaks = list(map(int,input().split()))
 more_kayaks = list(map(int,input().split()))
 
 for i in broken_kayaks:
-    kayaks[i-1] -= 1
+    kayaks[i] -= 1
 for i in more_kayaks:
-    kayaks[i-1] += 1
+    kayaks[i] += 1
 
-borrow_kayaks = [0]*n
 cnt = 0
-for num,kayak in enumerate(kayaks):
-    if borrow_kayaks[num] + kayak >= 1:
-        if borrow_kayaks[num] + kayak == 2:
-            try:
-                borrow_kayaks[num+1] = 1
-            except IndexError:
-                pass
-    else:
-        try:
-            if kayaks[num+1] == 2:
-                borrow_kayaks[num+1] = -1
-            else:
-                cnt += 1
-        except IndexError:
+for num in range(1,n+1):
+    if kayaks[num] == 0:
+        if kayaks[num-1] == 2:
+            kayaks[num-1] -= 1
+            kayaks[num] += 1
+        elif kayaks[num+1] == 2:
+            kayaks[num+1] -= 1
+            kayaks[num] += 1
+        else:
             cnt+=1
 print(cnt)

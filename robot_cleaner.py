f7a6005d9eb913639eaf3843e278b31b26ef11d6
chaeminsoo <chaems97@gmail.com>
2022-05-20 19:24:54 +0900

    Incomplete

---
+++ robot_cleaner.py
@@ -0,0 +1,15 @@
+n,m = map(int,input().split())
+r,c,d = map(int,input().split())
+board = []
+for _ in range(n):
+    board.append(list(map(int,input().split())))
+cnt = 1
+def moving():
+    return
+def cleanning():
+    return
+
+while moving:
+    cleanning()
+
+print(cnt)

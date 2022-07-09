569f3188bd1383173a686565a68624da38d0f0c3
chaeminsoo <chaems97@gmail.com>
2022-07-09 14:06:45 +0900

    BFS

--- laboratory3.py
+++ laboratory3.py
@@ -5,26 +5,22 @@ from collections import deque
 n,m = map(int,input().split())
 board  = []
 virus  =[]
+znt = 0
 for i in range(n):
     data = list(map(int,input().split()))
     for j in range(n):
         if data[j] == 2:
             virus.append((i,j))
+        if data[j] == 0: 
+            znt+=1 
     board.append(data)
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
-def checking(b):
-    for i in range(n):
-        for j in range(n):
-            if b[i][j] == 0:
-                return False
-    return True
-
-def bfs(g,virs):
-    if checking(g):
-        return 0
+def bfs(g,virs,znt):
+    if znt == 0:
+        return 0 
     q = deque()
     min_t = 0
     visited = [[False]*n for _ in range(n)]
@@ -35,20 +31,21 @@ def bfs(g,virs):
         visited[r][c] = True
     while q:
         x,y,t = q.popleft()
-        min_t = max(min_t,t)
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if nx >=0 and nx < n and ny >= 0 and ny < n and not visited[nx][ny] and g[nx][ny] != 1:
-                if g[nx][ny] == 2:
-                    q.append((nx,nx,t))
-                else:
-                    q.append((nx,nx,t+1))
+                if g[nx][ny] == 0:
+                    znt-=1
+                q.append((nx,ny,t+1))
+                min_t = max(min_t,t+1)
                 visited[nx][ny] = True
                 g[nx][ny] = 3
+        if znt == 0:
+            break
 
-    if checking(g):
+    if znt == 0:
         return min_t
     else:
         return -1
@@ -56,6 +53,12 @@ def bfs(g,virs):
 ans = 1e9
 cases = combinations(virus,m)
 for case in cases:
-    ans = min(bfs([i[:] for i in board],case),ans)
-
-print(ans)
+    ref = bfs([i[:] for i in board],case,znt)
+    if ref == -1:
+        continue
+    else:
+        ans = min(ref,ans)
+if ans == 1e9:
+    print(-1)
+else:
+    print(ans)

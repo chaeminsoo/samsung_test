05035588fbbf5d6658daeba4d7ef6811ad278d12
chaeminsoo <chaems97@gmail.com>
2022-08-15 15:38:45 +0900

    BFS(Implementation)

--- castle_defence.py
+++ castle_defence.py
@@ -12,13 +12,12 @@ archers = list(combinations(a,3))
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
-def attck(board,loc,d):
-    range_board = board[:]
-    range_board.append([0]*m)
-    r,c = loc
+def attack(b,r,c,d):
+    range_board = [[0]*m for _ in range(n+1)]
+    visited = [[False]*m for _ in range(n+1)]
     q = deque()
     q.append([r,c,0])
-    visited = [[False]*m for _ in range(n+1)]
+    visited[r][c] = True
     while q:
         x,y,v = q.popleft()
         range_board[x][y] = v
@@ -27,55 +26,58 @@ def attck(board,loc,d):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
+            if 0 <= nx < n+1 and 0 <= ny < m and not visited[nx][ny]:
                 q.append([nx,ny,v+1])
                 visited[nx][ny] = True
     hits = []
     for i in range(n):
         for j in range(m):
-            if range_board[i][j] == d:
-                hits.append([i,j])
-    hits.sort(key= lambda x: x[1])
-    return hits[0]
+            if range_board[i][j] <= d and b[i][j] == 1:
+                hits.append([i,j,range_board[i][j]])
+    hits.sort(key= lambda x:(x[2], x[1]))
+    if hits:
+        return hits[0][:2]
+    else:
+        return
 
-def checking(b):
-    for i in range(len(b)):
-        for j in range(len(b[0])):
+def done_(b):
+    for i in range(n):
+        for j in range(m):
             if b[i][j] == 1:
                 return False
     return True
 
-def e_moving(b):
-    new_b = b[:]
-    for i in range(len(b)):
-        for j in range(len(b[0])):
+def moving(b,ar):
+    ref = [[0]*m for _ in range(n)]
+    ref.append(ar)
+    for i in range(n):
+        for j in range(m):
+            if i == n-1 and b[i][j] == 1:
+                continue
             if b[i][j] == 1:
-                try:
-                    new_b[i+1][j] = 1
-                except IndexError:
-                    pass
-                b[i][j] = 0
-    for i in range(len(b)):
-        for j in range(len(b[0])):
-            b[i][j] += new_b[i][j]
-    return b
+                ref[i+1][j] = 1
+    return ref
 
 ans = 0
 for i in archers:
-    ref_board = board[:]
-    archer_loc = [0]*m
-    for j in i:
-        archer_loc[j] = 1
-    while checking:
-        ref_board.append(archer_loc)
+    arch_row = [0]*m
+    for j in range(m):
+        if j in i:
+            arch_row[j] = -1
+    ref_board = [ii[:] for ii in board]
+    ref_board.append(arch_row)
+    cnt = 0
+    while not done_(ref_board):
         hits = []
-        for j in archer_loc:
-            if j == 1:
-                hit = attck(ref_board,[n,j],d)
-                hits.append(hit)
+        for j, k in enumerate(ref_board[n]):
+            if k == -1:
+                hit = attack(ref_board,n,j,d)
+                if hit:
+                    hits.append(hit)
         for j,k in hits:
-            board[j][k] = 0
-            ans+=1
-        ref_board.pop()
-        ref_board = e_moving(ref_board)
+            if ref_board[j][k] == 1:
+                ref_board[j][k] = 0
+                cnt += 1
+        ref_board = moving(ref_board,arch_row)
+    ans = max(ans,cnt)
 print(ans)

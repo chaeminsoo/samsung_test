5105f3e070dc6ae123cdb77c47889ea53b49e599
chaeminsoo <chaems97@gmail.com>
2022-08-21 23:25:55 +0900

    Incomplete

--- attach_confetti.py
+++ attach_confetti.py
@@ -1,7 +1,13 @@
 # 17136
+# import time
+# stt = time.time()
 board = []
+ont = 0
 for i in range(10):
-    board.append(list(map(int,input().split())))
+    data = list(map(int,input().split()))
+    for j in data:
+        if j == 1: ont+=1
+    board.append(data)
 
 def box(x,y,num,board):
     if num == 0:
@@ -10,7 +16,7 @@ def box(x,y,num,board):
         ref = []
         for i in range(x,x+2):
             for j in range(y,y+2):
-                if board[i][j] == 0 or i > 10 or j > 10:
+                if i >= 10 or j >= 10 or board[i][j] == 0:
                     return False, False
                 else:
                     ref.append([i,j])
@@ -19,7 +25,7 @@ def box(x,y,num,board):
         ref = []
         for i in range(x,x+3):
             for j in range(y,y+3):
-                if board[i][j] == 0 or i > 10 or j > 10:
+                if i >= 10 or j >= 10 or board[i][j] == 0:
                     return False, False
                 else:
                     ref.append([i,j])
@@ -28,7 +34,7 @@ def box(x,y,num,board):
         ref = []
         for i in range(x,x+4):
             for j in range(y,y+4):
-                if board[i][j] == 0 or i > 10 or j > 10:
+                if i >= 10 or j >= 10 or board[i][j] == 0:
                     return False, False
                 else:
                     ref.append([i,j])
@@ -37,51 +43,113 @@ def box(x,y,num,board):
         ref = []
         for i in range(x,x+5):
             for j in range(y,y+5):
-                if board[i][j] == 0 or i > 10 or j > 10:
+                if i >= 10 or j >= 10 or board[i][j] == 0:
                     return False, False
                 else:
                     ref.append([i,j])
         return True, ref
 
-def a_check(b):
-    for i in range(10):
-        for j in range(10):
-            if b[i][j] == 1:
-                return False
-    return True
 def b_check(b):
     for i in b:
         if i > 5:
             return True
     return False
-
+#=======================================================
 ans = 1e9
-def dfs(x,y,board,b,cnt):
+def dfs(x,y,board,b,cnt,ont):
     global ans
-    if a_check(board):
+    if ont <= 0:
         ans = min(ans,cnt)
         return
     if b_check(b):
         return
+    if [x,y] == [9,9]:
+        return
     
-    for i in range(x,10):
-        for j in range(y,10):
-            if board[i][j] == 1:
+    if board[x][y] == 1:
+        
+        for k in range(5):
+            tf, targets = box(x,y,k,board)
+            if tf:
+                b[k]+=1
+                ont -= (k+1)**2
+                for tr,tc in targets:
+                    board[tr][tc] = 0
+                if y+1 < 10 and x+1 < 10:
+                    dfs(x,y+1,[c[:] for c in board],b,cnt+1,ont)
+                elif y+1 >= 10 and x+1 < 10:
+                    dfs(x+1,0,[c[:] for c in board],b,cnt+1,ont)
+
+                for tr,tc in targets:
+                    board[tr][tc] = 1
+                ont += (k+1)**2
+                b[k] -=1
+    else:
+        if y+1 < 10 and x+1 < 10:
+            dfs(x,y+1,[c[:] for c in board],b,cnt,ont)
+        elif y+1 >= 10 and x+1 < 10:
+            dfs(x+1,0,[c[:] for c in board],b,cnt,ont)
+
+# origin =======================================================
+# ans = 1e9
+# def dfs(x,board,b,cnt,ont):
+#     global ans
+#     if ont <= 0:
+#         ans = min(ans,cnt)
+#         return
+#     if b_check(b):
+#         return
+    
+#     for i in range(x,10):
+#         for j in range(10):
+#             if board[i][j] == 1:
                 
-                for k in range(5):
-                    tf, targets = box(i,j,k,board)
-                    if tf:
-                        b[k]+=1
-                        for tr,tc in targets:
-                            board[tr][tc] = 0
-                        dfs(i,j,[c[:] for c in board],b,cnt+1)
-                        for tr,tc in targets:
-                            board[tr][tc] = 1
-                        b[k] -=1
+#                 for k in range(5):
+#                     tf, targets = box(i,j,k,board)
+#                     if tf:
+#                         b[k]+=1
+#                         ont -= (k+1)**2
+#                         for tr,tc in targets:
+#                             board[tr][tc] = 0
+#                         dfs(i,[c[:] for c in board],b,cnt+1,ont)
+#                         for tr,tc in targets:
+#                             board[tr][tc] = 1
+#                         ont += (k+1)**2
+#                         b[k] -=1
+#=============================================================
+# ans = 1e9
+# def dfs(x,y,board,b,cnt,ont):
+#     global ans
+#     if ont <= 0:
+#         ans = min(ans,cnt)
+#         return
+#     if b_check(b):
+#         return
+#     if [x,y] == [9,9]:
+#         return
+                
+#     for k in range(5):
+#         tf, targets = box(x,y,k,board)
+#         if tf:
+#             b[k]+=1
+#             ont -= (k+1)**2
+#             for tr,tc in targets:
+#                 board[tr][tc] = 0
+#             dfs(x,[c[:] for c in board],b,cnt+1,ont)
+#             for tr,tc in targets:
+#                 board[tr][tc] = 1
+#             ont += (k+1)**2
+#             b[k] -=1
 
-
-dfs(0,0,board,[0,0,0,0,0],0)
+# for i in range(10):
+#     for j in range(10):
+#         if board[i][j] == 1:
+#             dfs(i,j,board,[0,0,0,0,0],0,ont)
+dfs(0,0,board,[0,0,0,0,0],0,ont)
+# dfs(0,board,[0,0,0,0,0],0,ont)
 if ans == 1e9:
     print(-1)
 else:
     print(ans)
+# edd = time.time()
+# print('t:', edd - stt)
